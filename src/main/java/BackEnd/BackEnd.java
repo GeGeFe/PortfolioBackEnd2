8daeefd19b8774cbb59cbb79fc35b885f66ea85e
GeGeFe <gabrielgf2002@yahoo.com.ar>
2022-04-09 18:46:10 -0300

    Filtro de autorización.

--- src/main/java/BackEnd/BackEnd.java
+++ src/main/java/BackEnd/BackEnd.java
@@ -9,6 +9,19 @@ public class BackEnd {
 	public static void main(String[] args) {
 		SpringApplication.run(BackEnd.class, args);
 	}
+// Esto es para configurar el acceso solo cuando hay usuario logueado. Sale de aquí: https://blog.softtek.com/es/autenticando-apis-con-spring-y-jwt
+/*        @EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/sesionInicioIrrestricto").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
+*/
 }
